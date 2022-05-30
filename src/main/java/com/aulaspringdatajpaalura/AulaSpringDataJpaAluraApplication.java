88e1a1c99452e85c66fb87f458de79d7e624b1cf
felipeperaltacheung <f33___@hotmail.com>
2022-05-30 16:24:38 -0300

    Projeto Maven criado + configuracao do pom.xml, dependencias spring data; starter web; mariadbJDBC; hibernate, e configuracao do application.properties para conexao com bd, e criacao da classe entidade cargo no package orm, inserindo atributos e as anotacoes.

---
+++ src/main/java/com/aulaspringdatajpaalura/AulaSpringDataJpaAluraApplication.java
@@ -0,0 +1,13 @@
+package com.aulaspringdatajpaalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringDataJpaAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringDataJpaAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aulaspringdatajpaalura/AulaSpringDataJpaAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaspringdatajpaalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringDataJpaAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
