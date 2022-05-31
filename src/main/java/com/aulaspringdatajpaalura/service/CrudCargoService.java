936199090f405354533b9e6a084fbe49f49fb1e9
felipeperaltacheung <f33___@hotmail.com>
2022-05-31 11:08:03 -0300

    Atualizando valores no banco dinamicamente pelo console; Criando metodo atualizar

--- src/main/java/com/aulaspringdatajpaalura/service/CrudCargoService.java
+++ src/main/java/com/aulaspringdatajpaalura/service/CrudCargoService.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 @Service
 public class CrudCargoService {
 
+    private Boolean system = true;
     private final CargoRepository cargoRepository;
 
     public CrudCargoService(CargoRepository cargoRepository){
@@ -16,6 +17,28 @@ public class CrudCargoService {
     }
 
     public void inicial(Scanner scanner){
+        while (system){
+            System.out.println("Qual acao de cargo deseja executar");
+            System.out.println("0 - Sair");
+            System.out.println("1 - salvar");
+            System.out.println("2 - atualizar");
+
+            int action = scanner.nextInt();
+            switch (action){
+                case 1:
+                    salvar(scanner);
+                    break;
+                case 2:
+                    atualizar(scanner);
+                    break;
+
+                default:
+                    system = false;
+                    break;
+            }
+
+        }
+
         salvar(scanner);
     }
     private void salvar(Scanner scanner){
@@ -26,4 +49,21 @@ public class CrudCargoService {
         cargoRepository.save(cargo);
         System.out.println("Salvo");
     }
+
+    private void atualizar(Scanner scanner){
+        System.out.println("Id");
+        int id = scanner.nextInt();
+        System.out.println("Descricao do cargo");
+        String descricao = scanner.next();
+
+        Cargo cargo = new Cargo();
+        cargo.setId(id);
+        cargo.setDescricao(descricao);
+        cargoRepository.save(cargo);
+        System.out.println("Atualizado");
+
+
+
+    }
+
 }
